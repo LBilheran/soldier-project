2350156e6a55c242d6d53480334226fabaf85fe4
LBilheran <loisbilheran@outlook.fr>
2025-02-16 17:23:32 +0100

    Add some changes

--- sw.js
+++ sw.js
@@ -7,15 +7,15 @@ self.addEventListener('install', (event) => {
         .then((cache) => {
           return cache.addAll([
             './',
-            'index.html',
-            'main.js',
-            'manifest.json',
-            'public/robot.png',
-            'public/explode.wav',
-            'public/music_AoW.mp3',
-            'public/quarry_01_1k.hdr',
-            'public/RobotExpressive.glb',
-            'public/Michelle.glb'
+            './index.html',
+            './main.js',
+            './manifest.json',
+            './public/robot.png',
+            './public/explode.wav',
+            './public/music_AoW.mp3',
+            './public/quarry_01_1k.hdr',
+            './public/RobotExpressive.glb',
+            './public/Michelle.glb'
          ]);
         })
         .then(() => {
