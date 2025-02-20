2257559142dc0108a30b42599e1934cd531f4ae5
LBilheran <loisbilheran@outlook.fr>
2025-02-20 18:14:04 +0100

    Follow example for github-pages

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: "src/",
-  publicDir: "../public/",
-  base: '/soldier-projet/',
-  clearScreen: false,
-    optimizeDeps: {
-        esbuildOptions: {
-            supported: {
-                'top-level-await': true
-            }
-        }
-    },
-    esbuild: {
-        supported: {
-            'top-level-await': true
-        }
-    },
-  server:
-    {
-      open: true
-    },
-  build:
-    {
-        outDir: "../dist",
-        emptyOutDir: true,
-        sourcemap: true,
-        chunkSizeWarningLimit: 1024
-    }
-});
+/**
+* @type {import('vite').UserConfig}
+*/
+export default {
+  base: process.env.NODE_ENV === 'production' ? '/soldier-project/' : ''
+}
